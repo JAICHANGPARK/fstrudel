19a275cb968eed1b81fe5d2b394ed8c617282857
Dreamwalker <aristojeff@gmail.com>
2026-01-23 20:00:18 +0900

    Create web_draw_canvas_stub.dart

---
+++ flutter_strudel/lib/src/web_draw_canvas_stub.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class StrudelScopeCanvas extends StatelessWidget {
+  const StrudelScopeCanvas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
