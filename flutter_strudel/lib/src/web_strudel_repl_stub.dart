5e8a5e7b0222fca66bcbe58a18cb8c97736087c0
Dreamwalker <aristojeff@gmail.com>
2026-01-23 19:59:55 +0900

    Create web_strudel_repl_stub.dart

---
+++ flutter_strudel/lib/src/web_strudel_repl_stub.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class StrudelReplEmbed extends StatelessWidget {
+  const StrudelReplEmbed({super.key, required this.code});
+
+  final String code;
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
