f9b83c710892cdce0c42d93b4eb4cd25a3353ff7
Dreamwalker <aristojeff@gmail.com>
2026-01-23 20:00:01 +0900

    Create web_strudel_iframe_stub.dart

---
+++ flutter_strudel/lib/src/web_strudel_iframe_stub.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class StrudelIframeEmbed extends StatelessWidget {
+  const StrudelIframeEmbed({super.key, required this.src});
+
+  final String src;
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
